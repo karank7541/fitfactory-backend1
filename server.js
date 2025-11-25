fb8a2f07acba4f2d28a9b3548f90d78403a4cd72
fitfactory-gym <fitfactoryranchi@gmail.com>
2025-11-25 08:01:42 +0530

    port update

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use("/api/feedback", feedbackRoutes);
 app.use("/api/admin", adminRoutes);
 
 // Railway ke liye important
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`✔ Server running on ${PORT}`);
 });
