c8318ca6365ee59512c22fd9a07ce246cfae826b
fitfactory-gym <fitfactoryranchi@gmail.com>
2025-11-25 07:54:17 +0530

    maine fir se update kiya

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -15,29 +14,20 @@ connectToDb();
 
 const app = express();
 
-// Allow all origins
 app.use(cors());
 app.use(express.json());
 
-// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("FitFactory Backend Running...");
 });
 
-// USER AUTH + OTP ROUTES
 app.use("/api/auth", authRoutes);
 app.use("/api/auth", otpRoutes);
-
-// CONTACT ROUTES
 app.use("/api/contact", contactRoutes);
-
-// FEEDBACK ROUTES
 app.use("/api/feedback", feedbackRoutes);
-
-// ADMIN ROUTES
 app.use("/api/admin", adminRoutes);
 
-// ✔ IMPORTANT FOR RAILWAY
+// Railway ke liye important
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`✔ Server running on ${PORT}`);
